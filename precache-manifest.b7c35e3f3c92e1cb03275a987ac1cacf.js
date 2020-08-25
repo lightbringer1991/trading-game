8799514f44308697aacf71e9fec8c6c9e7bec214
Tuan Nguyen <lightbringer1991@yahoo.com>
2020-08-25 20:10:05 +1000

    Updates

---
+++ precache-manifest.b7c35e3f3c92e1cb03275a987ac1cacf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85eaa0a3632031e8f8bc9e0ee0fdac59",
+    "url": "/trading-game/index.html"
+  },
+  {
+    "revision": "9605d31a963660445246",
+    "url": "/trading-game/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "62930101db0721a4bada",
+    "url": "/trading-game/static/css/main.11ef1999.chunk.css"
+  },
+  {
+    "revision": "9605d31a963660445246",
+    "url": "/trading-game/static/js/2.96b223a7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/trading-game/static/js/2.96b223a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62930101db0721a4bada",
+    "url": "/trading-game/static/js/main.b966c903.chunk.js"
+  },
+  {
+    "revision": "434a676ce69b5710a3ed",
+    "url": "/trading-game/static/js/runtime-main.b8abf3a2.js"
+  }
+]);
